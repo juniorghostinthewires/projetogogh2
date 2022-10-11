597588891856f4c0f0800b6c376b61abae5877ce
Junior de Sousa <68577415+juniorghostinthewires@users.noreply.github.com>
2022-10-11 17:24:17 -0300

    bora bill final

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
-      <H2>Interessado em conhecer mais sobre nosso projeto? Deixei seu e-mail para que possamos entrar em contato contigo!</H2> 
+      <H3>Interessado em conhecer mais sobre nosso projeto? Deixei seu e-mail para que possamos entrar em contato contigo!</H3> 
         <div class="mb-12 xl:mb-0">
           <h4 v-if="isSignedUp">Muito obrigado, entraremos em contato brevemente</h4>
 
@@ -28,7 +28,7 @@
               class="flex-shrink-0 bg-blue-500 hover:bg-blue-700 border-blue-500 hover:border-blue-700 text-sm border-4 text-white py-1 px-2 rounded"
               type="submit"
             >
-              Logue-se
+              Entrar em contato
             </button>
           </form>
         </div>
