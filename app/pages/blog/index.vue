0d5e43897eec21aca4480fc0a4cb496b5908a7b6
Junior de Sousa <68577415+juniorghostinthewires@users.noreply.github.com>
2022-10-11 12:09:44 -0300

    Edição no texto do primeiro bloco

--- app/pages/blog/index.vue
+++ app/pages/blog/index.vue
@@ -1,7 +1,7 @@
 <template>
   <section class="blog">
     <div class="py-8 md:py-16 text-center">
-      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">O Projeto/h1>
+      <h1 class="text-lg md:text-xl lg:text-4xl xl:text-6xl">O Projeto </h1>
       <h2 class="text-base md:text-lg lg:text-xl xl:text-2xl">
        Uma breve descrição de nossas motivações, história, e, projeto.
       </h2>
